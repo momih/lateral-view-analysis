9741190138ee0ef97ab3f42e74830f637b864038
Hashir <mohammadhashir94@gmail.com>
2019-08-01 12:16:16 -0400

    move dropout function into separate file

---
+++ models/dropout.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+
+
+def add_dropout_rec(module, p):
+    if isinstance(module, nn.modules.conv.Conv2d) or isinstance(module, nn.modules.Linear):
+        return nn.Sequential(module, nn.Dropout(p))
+    for name in module._modules.keys():
+        module._modules[name] = add_dropout_rec(module._modules[name], p=p)
+    return module
+
+
+def add_dropout(net, p=0.1, model='densenet'):
+    if model == 'densenet':
+        for name in net.features._modules.keys():
+            if name != "conv0":
+                net.features._modules[name] = add_dropout_rec(net.features._modules[name], p=p)
+
+    elif model == 'hemis':
+        for x in ('branches', 'combined'):
+            module = net._modules[x]
+            for name in module._modules.keys():
+                if name != "conv0":
+                    module._modules[name] = add_dropout_rec(module._modules[name], p=p)
+
+    elif model == 'multitask':
+        for name in net.frontal_model.features._modules.keys():
+            if name != "conv0":
+                net.frontal_model.features._modules[name] = add_dropout_rec(net.frontal_model.features._modules[name],
+                                                                            p=p)
+        net.frontal_model.classifier = add_dropout_rec(net.frontal_model.classifier, p=p)
+
+        for name in net.lateral_model.features._modules.keys():
+            if name != "conv0":
+                net.lateral_model.features._modules[name] = add_dropout_rec(net.lateral_model.features._modules[name],
+                                                                            p=p)
+        net.lateral_model.classifier = add_dropout_rec(net.lateral_model.classifier, p=p)
+
+    net.classifier = add_dropout_rec(net.classifier, p=p)
+    return net
