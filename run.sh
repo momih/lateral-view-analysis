2524cca9ab575f9156b0f43074fb1a0f8cbf4a71
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-08-23 09:48:08 -0400

    update script

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ hostname
 export LANG=C.UTF-8
 source $HOME/.bashrc
 
-DATADIR=~/scratch/PC/images-224
-#DATADIR=$SLURM_TMPDIR/images-224
-#rsync -a --info=progress2 images-224.tar $SLURM_TMPDIR/
-#time tar xf $SLURM_TMPDIR/images-224.tar -C $SLURM_TMPDIR/ --strip=4
+#DATADIR=~/scratch/PC/images-224
+DATADIR=$SLURM_TMPDIR/images-224
+rsync -a --info=progress2 ~/scratch/PC/images-224.tar $SLURM_TMPDIR/
+time tar xf $SLURM_TMPDIR/images-224.tar -C $SLURM_TMPDIR/ --strip=4
 
 python3 -u train.py $DATADIR ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/joint_PA_L.csv ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/splits_PA_L_{}.pkl /lustre04/scratch/cohenjos/PC-output/joe-pa-densenet-s{} --batch_size=64 $@
 
