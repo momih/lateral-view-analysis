523a352b8a1987327288679ab953411dd94a98b8
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-08-23 10:01:01 -0400

    update script

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ DATADIR=$SLURM_TMPDIR/images-224
 time rsync -a --info=progress2 ~/scratch/PC/images-224.tar $SLURM_TMPDIR/
 time tar xf $SLURM_TMPDIR/images-224.tar -C $SLURM_TMPDIR/ --strip=4
 
-python3 -u train.py $DATADIR ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/joint_PA_L.csv ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/splits_PA_L_{}.pkl /lustre04/scratch/cohenjos/PC-output/joe-pa-densenet-s{} --batch_size=64 $@
+python3 -u train.py $DATADIR ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/joint_PA_L.csv ~/projects/rpp-bengioy/jpcohen/PADCHEST_SJ/labels_csv/splits_PA_L_{}.pkl /lustre04/scratch/cohenjos/PC-output/joe-s{} --batch_size=64 $@
 
 
 
